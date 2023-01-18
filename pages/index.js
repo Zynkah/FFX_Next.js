63edc7d982babb281baff4c9df47bea0f1571776
Zynkah <ffgurl07@yahoo.com>
2023-01-18 15:37:26 -0800

    Homepage Blog Links

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import Date from "../components/date";
 import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
@@ -20,21 +21,23 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <hr />
         <p>Welcome To Spira</p>
         <p>
           Slowly I will turn this sample page into my FFX fan page. Stay tuned!
         </p>
+        <hr />
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
