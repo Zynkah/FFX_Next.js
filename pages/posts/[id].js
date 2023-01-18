1dc459a4a88c29a571e9b5c0691b6c9d9317a0bd
Zynkah <ffgurl07@yahoo.com>
2023-01-18 15:18:02 -0800

    Blog Styling

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -21,13 +24,18 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+        <br />
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <hr />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
