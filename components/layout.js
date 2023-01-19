b203ca2730ca7ca9d8ecef770beec05a63b7bb3b
Zynkah <ffgurl07@yahoo.com>
2023-01-18 19:56:20 -0800

    API Route Setup

--- components/layout.js
+++ components/layout.js
@@ -5,17 +5,14 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Final Fantasy X";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Final Fantasy X";
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
+        <meta name="description" content="Final Fantasy X Information" />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
