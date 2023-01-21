7ef96f28eef19a6da4f55eaa984515474d535683
Zynkah <ffgurl07@yahoo.com>
2023-01-20 18:13:20 -0800

    Aeons Added

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,8 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Link href={`/characters/`}>Characters</Link>
+      <Link href={`/characters/`}>Characters </Link>
+      <Link href={`/aeons/`}> Aeons </Link>
       <section className={utilStyles.headingMd}>
         <hr />
         <h1>Welcome To Spira</h1>
