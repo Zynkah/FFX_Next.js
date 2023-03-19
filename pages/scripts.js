922b00e0c0ae90af706d993397cf6caa903b6c96
Zynkah <ffgurl07@yahoo.com>
2023-03-18 18:34:12 -0700

    Script Update

--- pages/scripts.js
+++ pages/scripts.js
@@ -11,104 +11,92 @@ export default function Scripts() {
       <h2>Scripts</h2>
 
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Paper>
-            <Image
-              src="/images/Spiran_Script.webp"
-              height={400}
-              width={515}
-              style={{
-                margin: "auto",
-              }}
-              alt="Spiran Script"
-            />
-          </Paper>
+        <Grid item xs={12} md={6} marginBottom={10}>
+          <Image
+            src="/images/Spiran_Script.webp"
+            height={400}
+            width={515}
+            style={{
+              margin: "auto",
+            }}
+            alt="Spiran Script"
+          />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
-            <h2>Spiran Script</h2>
-            <Image
-              src="/images/Sphere-Broadcast-Billboard.webp"
-              height={200}
-              width={231}
-              style={{
-                margin: "auto",
-              }}
-              alt="sphere broadcast billboard"
-            />
-            <p>
-              A curvy script used all across Spira. Characters exist for each
-              uppercase and lowercase letter. This language can be found in many
-              locations. On signs, Blitzball team logos, all over the Luca
-              Stadium, and more.
-            </p>
-          </Paper>
+        <Grid item xs={12} md={6} marginBottom={10}>
+          <h2>Spiran Script</h2>
+          <Image
+            src="/images/Sphere-Broadcast-Billboard.webp"
+            height={200}
+            width={231}
+            style={{
+              margin: "auto",
+            }}
+            alt="sphere broadcast billboard"
+          />
+          <p>
+            A curvy script used all across Spira. Characters exist for each
+            uppercase and lowercase letter. This language can be found in many
+            locations. On signs, Blitzball team logos, all over the Luca
+            Stadium, and more.
+          </p>
         </Grid>
 
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
-            <h2>Al Bhed Script</h2>
-            <Image
-              src="/images/Al-Bhed-Primer-Artwork.webp"
-              height={200}
-              width={296}
-              style={{
-                margin: "auto",
-              }}
-              alt="Al Bhed Primer"
-            />
-            <p>
-              The script used by the Al Bhed, distinctive for its combination of
-              two to four fragments to make each character. Uppercase and
-              lowercase variations are very similar across the characters.
-            </p>
-          </Paper>
+        <Grid item xs={12} md={6} marginBottom={10}>
+          <h2>Al Bhed Script</h2>
+          <Image
+            src="/images/Al-Bhed-Primer-Artwork.webp"
+            height={200}
+            width={296}
+            style={{
+              margin: "auto",
+            }}
+            alt="Al Bhed Primer"
+          />
+          <p>
+            The script used by the Al Bhed, distinctive for its combination of
+            two to four fragments to make each character. Uppercase and
+            lowercase variations are very similar across the characters.
+          </p>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
-            <Image
-              src="/images/Al_Bhed_Script.webp"
-              height={400}
-              width={597}
-              style={{
-                margin: "auto",
-              }}
-              alt="Al Bhed Script"
-            />
-          </Paper>
+        <Grid item xs={12} md={6} marginBottom={10}>
+          <Image
+            src="/images/Al_Bhed_Script.webp"
+            height={400}
+            width={597}
+            style={{
+              margin: "auto",
+            }}
+            alt="Al Bhed Script"
+          />
         </Grid>
 
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
-            <Image
-              src="/images/Yevon_Script.webp"
-              height={400}
-              width={515}
-              style={{
-                margin: "auto",
-              }}
-              alt="Yevon Script"
-            />
-          </Paper>
+        <Grid item xs={12} md={6} marginBottom={10}>
+          <Image
+            src="/images/Yevon_Script.webp"
+            height={400}
+            width={515}
+            style={{
+              margin: "auto",
+            }}
+            alt="Yevon Script"
+          />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
-            <h2>Yevon Script</h2>
-            <Image
-              src="/images/Kilika-temple-artwork-ffx.webp"
-              height={200}
-              width={251}
-              style={{
-                margin: "auto",
-              }}
-              alt="inside Kilika temple"
-            />
-            <p>
-              Characters resembling Sanskrit that are used predominantly in
-              Yevon institutions. Some characters have their own individual
-              meanings, such as 'fire' or 'Yevon'.
-            </p>
-          </Paper>
+        <Grid item xs={12} md={6} marginBottom={10}>
+          <h2>Yevon Script</h2>
+          <Image
+            src="/images/Kilika-temple-artwork-ffx.webp"
+            height={200}
+            width={251}
+            style={{
+              margin: "auto",
+            }}
+            alt="inside Kilika temple"
+          />
+          <p>
+            Characters resembling Sanskrit that are used predominantly in Yevon
+            institutions. Some characters have their own individual meanings,
+            such as 'fire' or 'Yevon'.
+          </p>
         </Grid>
       </Grid>
 
