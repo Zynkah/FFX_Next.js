e0d12c49b65bf310947264f530b0f9389f2a818b
Zynkah <ffgurl07@yahoo.com>
2023-03-26 16:00:14 -0700

    Blitzball Page Updated

--- pages/blitzball.js
+++ pages/blitzball.js
@@ -1,11 +1,11 @@
 import Layout from "../components/layout";
 import Image from "next/image";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Link from "next/link";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
+import { Card, Text } from "@nextui-org/react";
 
 export default function Blitzball() {
   return (
@@ -15,9 +15,10 @@ export default function Blitzball() {
       <Image
         src="/images/Blitzball_Logo.jpg"
         height={400}
-        width={422}
+        width={438}
         style={{
           margin: "auto",
+          borderRadius: "5px",
         }}
         alt="Blitzball Logo"
       />
@@ -33,121 +34,118 @@ export default function Blitzball() {
 
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Besaid_Aurochs.jpg"
                   height={400}
-                  width={383}
+                  width={402}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Besaid Aurochs"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Besaid Aurochs</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   The Aurochs hail from the seaside village of Besaid, and at
                   the beginning of Final Fantasy X, are under Wakka's captaincy.
                   They are the only team in Spira to have never passed the first
                   round of the Luca blitz tournament. When Tidus joins in
                   exchange for passage, their success rate soars. The player has
                   control over the Besaid Aurochs following the events in Luca.
-                  <hr />
-                  <Link href={`../blitzball/aurochs/`}>Team Statistics</Link>
-                </p>
+                </Text>
+                <Link href={`../blitzball/aurochs/`}>Team Statistics</Link>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Luca_Goers.jpg"
                   height={400}
-                  width={589}
+                  width={645}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Luca Goers"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Luca Goers</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   At the opening of Final Fantasy X, the Luca Goers reign as the
                   Spira champions. They are captained by the confident and brash
                   forward, Bickson. As they represent the city with the only
                   blitz stadium, they are regarded as the most beloved team in
                   Spira. The team members can be found in and around Luca
                   stadium, if the player wishes to add them to their team.
-                  <hr />
-                  <Link href={`../blitzball/goers/`}>Team Statistics</Link>
-                </p>
+                </Text>
+                <Link href={`../blitzball/goers/`}>Team Statistics</Link>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Kilika_Beasts.jpg"
                   height={400}
-                  width={431}
+                  width={504}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Kilika Beasts"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Kilika Beasts</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   The Kilika Beasts hail from the small seaside town of Kilika.
                   High Summoner Ohalland used to play for the team before he
                   became a summoner. The Kilika Beasts' team members are found
                   throughout Kilika from the port to the temple.
-                  <hr />
-                  <Link href={`../blitzball/beasts/`}>Team Statistics</Link>
-                </p>
+                </Text>
+                <Link href={`../blitzball/beasts/`}>Team Statistics</Link>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Al_Bhed_Psyches.jpg"
                   height={400}
-                  width={592}
+                  width={675}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Al Bhed Psyches"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Al Bhed Psyches</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   The Al Bhed Psyches represent the Al Bhed race. During the
                   Luca tournament of Final Fantasy X, they attempt to rig their
                   opening match against the Besaid Aurochs by kidnapping Yuna.
@@ -156,146 +154,141 @@ export default function Blitzball() {
                   available when the player has access to the airship, which is
                   right before entering Bevelle, and permanently after the
                   player has finished the events in Zanarkand.
-                  <hr />
-                  <Link href={`../blitzball/psyches/`}>Team Statistics</Link>
-                </p>
+                </Text>
+                <Link href={`../blitzball/psyches/`}>Team Statistics</Link>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Ronso_Fangs.jpg"
                   height={400}
-                  width={795}
+                  width={805}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Ronso Fangs"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Ronso Fangs</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   The Ronso Fangs represent the Ronso race. Due to their
                   physicality, they rely on power and endurance while lacking in
                   speed. The Ronso Fangs members are found at the Luca Docks.
-                  <hr />
-                  <Link href={`../blitzball/fangs/`}>Team Statistics</Link>
-                </p>
+                </Text>
+                <Link href={`../blitzball/fangs/`}>Team Statistics</Link>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Guado_Glories.jpg"
                   height={400}
-                  width={409}
+                  width={446}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Guado Glories"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Guado Glories</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   The Guado Glories represent the Guado race, and hail from
                   Guadosalam. Their players contrast the Ronso Fangs with high
                   speed at the expense of HP, Attack, and Endurance. The Guado
                   Glories team can be found in Guadosalam.
-                  <hr />
-                  <Link href={`../blitzball/glories/`}>Team Statistics</Link>
-                </p>
+                </Text>
+                <Link href={`../blitzball/glories/`}>Team Statistics</Link>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Zanarkand_Duggles.jpg"
                   height={400}
                   width={851}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Zanarkand Duggles"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Zanarkand Duggles</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   The Zanarkand Duggles are another team competing in the Jecht
                   Memorial Cup, featuring black and red outfits. Little is known
                   about the team other than that they "play dirty".
-                </p>
-                <hr />
+                </Text>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Zanarkand_Abes.jpg"
                   height={400}
-                  width={435}
+                  width={423}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Zanarkand Abes"
                 />
               </AccordionSummary>
               <AccordionDetails>
                 <h2>Zanarkand Abes</h2>
-                <hr />
-                <p>
+                <Card.Divider />
+                <Text>
                   The Zanarkand Abes are Tidus's team in Dream Zanarkand, where
                   he is the star player. They have yellow and black outfits. His
                   father, Jecht, also played for the Abes, a shadow Tidus
                   struggles to step out of. They are only available to play
                   against in Final Fantasy X-2.
-                </p>
-                <hr />
+                </Text>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Yocun_Nomads.jpg"
                   height={400}
                   width={396}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Yocun Nomads"
                 />
@@ -304,20 +297,20 @@ export default function Blitzball() {
                 <h3>Yocun Nomads (Unused Team)</h3>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12} md={6}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <Image
-                  className="cardImage"
                   src="/images/Bevelle_Bells.jpg"
                   height={400}
                   width={344}
                   style={{
                     margin: "auto",
+                    borderRadius: "5px",
                   }}
                   alt="Bevelle Bells"
                 />
@@ -326,50 +319,63 @@ export default function Blitzball() {
                 <h3>Bevelle Bells (Unused Team)</h3>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
 
         <Grid item xs={12}>
-          <Paper elevation={3}>
+          <Card isPressable isHoverable variant="bordered">
             <Accordion>
               <AccordionSummary>
                 <h2>Recruiting and Scouting</h2>
-                <p>
-                  There are many players scattered throughout Spira who you can
-                  scout and add to your team.
-                </p>
               </AccordionSummary>
               <AccordionDetails>
+              <Card.Divider />
+                <Text>
+                  There are many players scattered throughout Spira who you can
+                  scout and add to your team.
+                </Text>
                 <h3>Free Agents</h3>
-                <ul>
-                  <li>Brother</li>
-                  <li>Wedge</li>
-                  <li>Kiyuri</li>
-                  <li>Kyou</li>
-                  <li>Zalitz</li>
-                  <li>Shaami</li>
-                  <li>Shuu</li>
-                  <li>Jumal</li>
-                  <li>Svanda</li>
-                  <li>Zev Ronso</li>
-                  <li>Tatts</li>
-                  <li>Durren</li>
-                  <li>Nedus</li>
-                  <li>Naida</li>
-                  <li>Biggs</li>
-                  <li>Vilucha</li>
-                  <li>Mifurey</li>
-                  <li>Miyu</li>
-                  <li>Mep</li>
-                  <li>Yuma Guado</li>
-                  <li>Linna</li>
-                  <li>Rin</li>
-                  <li>Ropp</li>
-                  <li>Wakka</li>
-                </ul>
+                <Grid container spacing={2}>
+                <Grid item xs={12} md={4}>
+                  <ul>
+                    <li>Brother</li>
+                    <li>Wedge</li>
+                    <li>Kiyuri</li>
+                    <li>Kyou</li>
+                    <li>Zalitz</li>
+                    <li>Shaami</li>
+                    <li>Shuu</li>
+                    <li>Jumal</li>
+                  </ul>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <ul>
+                    <li>Svanda</li>
+                    <li>Zev Ronso</li>
+                    <li>Tatts</li>
+                    <li>Durren</li>
+                    <li>Nedus</li>
+                    <li>Naida</li>
+                    <li>Biggs</li>
+                    <li>Vilucha</li>
+                  </ul>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <ul>
+                    <li>Mifurey</li>
+                    <li>Miyu</li>
+                    <li>Mep</li>
+                    <li>Yuma Guado</li>
+                    <li>Linna</li>
+                    <li>Rin</li>
+                    <li>Ropp</li>
+                    <li>Wakka</li>
+                  </ul>
+                </Grid>
+                </Grid>
               </AccordionDetails>
             </Accordion>
-          </Paper>
+          </Card>
         </Grid>
       </Grid>
     </Layout>
