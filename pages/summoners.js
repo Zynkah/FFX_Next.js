0401e911db274d3bb413c876b2b83bfb755d347f
Zynkah <ffgurl07@yahoo.com>
2023-03-26 18:31:11 -0700

    Summoner Page Updated

--- pages/summoners.js
+++ pages/summoners.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
-import { Card, Text } from "@nextui-org/react";
+import { Card } from "@nextui-org/react";
 
 export default function Summoners() {
   return (
@@ -36,7 +36,7 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Lady Yunalesca</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A legendary heroine who devised the Final Summoning thousands
                   of years prior to the events of the story. In the present era,
                   she resides in Zanarkand as an unsent. As the daughter of the
@@ -44,7 +44,7 @@ export default function Summoners() {
                   destroyed as long as her father Yu Yevon's shade remains as
                   its core. Using this knowledge, she bestows the Final
                   Summoning upon worthy summoners.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -68,11 +68,11 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Gandof</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   Spira's first high summoner, he defeated Sin through the Final
                   Summoning four hundred years ago. Famed for sealing the evil
                   Qactuars in stones across the Thunder Plains.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -96,11 +96,11 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Ohalland</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A high summoner from Kilika Island who defeated Sin 230 years
                   ago. A vaunted blitzball player for the Kilika Beasts who took
                   on a godlike status within the sport.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -124,11 +124,11 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Yocun</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A high summoner who defeated Sin one hundred years ago and
                   brought about a Calm. Originally a member of the Crusaders,
                   she trained continuously in the valley of the Calm Lands.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -152,7 +152,7 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Lord Braska</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   Yuna's late father and the fourth high summoner of Spira. A
                   former priest who became a summoner after Sin killed his wife,
                   an Al Bhed. Ten years before the events of the story, Braska
@@ -160,7 +160,7 @@ export default function Summoners() {
                   generation's Sin through the Final Summoning. He lost his life
                   in the process, leaving deep scars on the daughter he left
                   behind.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -184,7 +184,7 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Yuna</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A summoner who is following in the footsteps of her father,
                   the late high summoner Braska. At 17 years old, she embarks on
                   her pilgrimage to obtain the Final Summoning to defeat Sin. On
@@ -194,7 +194,7 @@ export default function Summoners() {
                   find her own way to beat Sin. She is branded a traitor to
                   Yevon but still continues her journey and defeats Sin for good
                   at the end.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -218,7 +218,7 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Yu Yevon</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   The leader of ancient Zanarkand. He is lady Yunalesca's father
                   and the creator of Sin. One thousand years ago Zanarkand and
                   Bevelle were at war. Although Zanarkand had powerful
@@ -231,7 +231,7 @@ export default function Summoners() {
                   creates Sin to protect his Dream Zanarkand. Sin's ultimate
                   goal is to destroy all machina and all large settlements to
                   stall technological evolution, to avoid its demise once more.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -255,7 +255,7 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Lenne</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   Summoner who lived back in the time of the ancient war before
                   Sin was created. She was a pop-star summoner that was drafted
                   in the war, her lover Shuyin infiltrated Bevelle to end the
@@ -263,7 +263,7 @@ export default function Summoners() {
                   Lenne followed him and died trying to stop him. They were
                   caught by Bevelle's security forces and they unleashed a
                   barrage of bullets upon them.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -287,7 +287,7 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Seymour</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A capable individual who became a maester of Yevon's teachings
                   at a young age, Seymour faced persecution all his life due to
                   his mixed heritage as the child of a human mother and a Guado
@@ -297,7 +297,7 @@ export default function Summoners() {
                   the Guado. Well aware that the Final Aeon must become the next
                   Sin, he wants to exploit Yuna to become Sin itself so he can
                   destroy Spira.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -321,12 +321,12 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Isaaru</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A serious and well-mannered summoner with a cool appearance.
                   He idolized Braska from a young age and is friendly towards
                   his daughter Yuna. Nevertheless, he is forced to fight he on
                   the order of the temples.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -350,12 +350,12 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Dona</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A sexy summoner who views Yuna as her rival. She is proud of
                   the bond between her and Barthello, her lover and guardian.
                   Though she lacks honesty and affects an air of
                   self-importance, Dona is actually a sweet girl at her core.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -379,12 +379,12 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Belgemine</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   A wandering summoner. Having been a summoner far longer than
                   Yuna, she challenges her to multiple battles across the land
                   to better train her aeons. In truth she is an unsent who
                   remains on Spira to train up-and-coming summoners.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -408,13 +408,13 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Ginnem</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   The first summoner to take on Lulu as a guardian. Two years
                   ago she set out on a pilgrimage for the Final Summoning but
                   used up all her strength communing with the fayth of the aeon
                   Yojimbo. She remains on Spira as an unsent, dwelling in the
                   cavern of that fayth, in the Calm Lands.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -438,12 +438,12 @@ export default function Summoners() {
               <AccordionDetails>
                 <h3>Zuke</h3>
                 <Card.Divider />
-                <Text>
+                <p>
                   The second summoner who accepted Lulu as his guardian. He
                   failed in his pilgrimage half a year ago in the Calm Lands. He
                   wishes only for Lulu's happiness and risks warning her when
                   her party is pursued by Yevon.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
@@ -457,11 +457,11 @@ export default function Summoners() {
               </AccordionSummary>
               <AccordionDetails>
               <Card.Divider />
-                <Text>
+                <p>
                   She is not named and only very briefly mentioned in the story.
                   All that is known is that she was younger than 22 when she
                   passed.
-                </Text>
+                </p>
               </AccordionDetails>
             </Accordion>
           </Card>
