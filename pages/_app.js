47a826b375fc2dcb5bf568095024460353b3940f
Zynkah <ffgurl07@yahoo.com>
2023-03-31 16:17:53 -0700

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import * as React from 'react';
+import * as React from "react";
 import "../styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
       <Component {...pageProps} />
+      <Analytics />
     </NextUIProvider>
   );
 }
