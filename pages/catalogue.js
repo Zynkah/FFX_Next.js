a321518d247360e20494c307435bd1f901858bc6
Zynkah <ffgurl07@yahoo.com>
2023-03-31 17:23:30 -0700

    Catalogue Updated

--- pages/catalogue.js
+++ pages/catalogue.js
@@ -1,10 +1,16 @@
 import Layout from "../components/layout";
 import Link from "next/link";
 import Grid from "@mui/material/Grid";
+import { Divider } from "@nextui-org/react";
 
 export default function Catalogue() {
   return (
     <Layout>
+      <br />
+      <br />
+      <Divider><h3>Page Links</h3></Divider>
+      <br />
+      <br/>
       <Grid container spacing={2}>
         <Grid item xs={12} md={4}>
           <ul>
@@ -70,6 +76,15 @@ export default function Catalogue() {
           </ul>
         </Grid>
       </Grid>
+      <br />
+      <br />
+      <Divider><h3>Creator Links</h3></Divider>
+      <br />
+      <br/>
+      <Link href="https://www.paypal.com/donate/?business=C5B9D466CUT6N&no_recurring=0&item_name=If+you+want+to+support+my+website+I+would+really+appreciate+it%21+I+do+this+all+on+my+own+for+free%2C+thank+you%21&currency_code=USD">Donate |</Link>
+      <Link href="https://zynkah-merch-2.creator-spring.com/"> Merch |</Link>
+      <Link href="https://github.com/Zynkah?tab=repositories"> GitHub |</Link>
+      <Link href="https://www.linkedin.com/in/zena-creps/"> LinkedIn</Link>
     </Layout>
   );
 }
