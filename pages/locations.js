2de96c437d9c6cb29eb48a7faa1fadc891cb6420
Zynkah <ffgurl07@yahoo.com>
2023-04-09 20:59:23 -0500

    Location Links Updated

--- pages/locations.js
+++ pages/locations.js
@@ -1,18 +1,75 @@
 import Layout from "../components/layout";
 import Image from "next/image";
-import Link from "next/link";
 import Grid from "@mui/material/Grid";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import { Card } from "@nextui-org/react";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
 export default function Locations() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <Layout>
       <h1>Locations</h1>
-      <Link href={`/temples/`}> Temples |</Link>
-      <Link href={`/agencies/`}> Travel Agencies</Link>
+      <Box sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: "divider",
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons
+            aria-label="basic tabs"
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            <Tab label="Temples" href={`/temples/`} />
+            <Tab label="Agencies" href={`/agencies/`} />
+          </Tabs>
+        </Box>
       <hr />
       <br />
       <Image
@@ -756,6 +813,7 @@ export default function Locations() {
           </Card>
         </Grid>
       </Grid>
+      </Box>
     </Layout>
   );
 }
