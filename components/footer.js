6ea5df92719a2c16d735087d6dbca148b6a74b77
Zynkah <ffgurl07@yahoo.com>
2023-06-27 19:36:42 -0500

    updates

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,20 @@
-import { Container } from "@nextui-org/react";
+import { Container, Divider, Typography } from "@nextui-org/react";
 // import Link from "next/link";
 
 export default function Footer() {
   return (
-      <Container>
-        {/* <Link>Donate</Link> */}
-      </Container>
+    <Container>
+      {/* <Divider>
+        <Typography variant="h5" component="h2">
+          Creator Links
+        </Typography>
+      </Divider>
+      <Link href="https://www.paypal.com/donate/?business=C5B9D466CUT6N&no_recurring=0&item_name=If+you+want+to+support+my+website+I+would+really+appreciate+it%21+I+do+this+all+on+my+own+for+free%2C+thank+you%21&currency_code=USD">
+        Donate |
+      </Link>
+      <Link href="https://zynkah-merch-2.creator-spring.com/"> Merch |</Link>
+      <Link href="https://github.com/Zynkah?tab=repositories"> GitHub |</Link>
+      <Link href="https://www.linkedin.com/in/zena-creps/"> LinkedIn</Link> */}
+    </Container>
   );
 }
