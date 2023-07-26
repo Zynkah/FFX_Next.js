3760d951f321a2675a3d3d9ca54935c5e8894198
Zynkah <ffgurl07@yahoo.com>
2023-07-26 16:53:58 -0500

    blitzball update

--- pages/blitzball/beasts.js
+++ pages/blitzball/beasts.js
@@ -1,7 +1,7 @@
 import Layout from "../../components/layout";
 import Image from "next/image";
 import { Typography } from "@mui/material";
-import { Table } from "@nextui-org/react";
+import { Table, Button } from "@nextui-org/react";
 
 export default function Aurochs() {
   return (
@@ -13,61 +13,124 @@ export default function Aurochs() {
         Team Members
       </Typography>
       <Table
-        aria-label="Example static collection table"
+        aria-label="Beast Team Members table"
         css={{
           height: "auto",
           minWidth: "100%",
         }}
         selectionMode="single"
+        color="secondary"
+        bordered
+        lined
       >
         <Table.Header>
           <Table.Column align="center">Photo</Table.Column>
           <Table.Column align="center">Name</Table.Column>
           <Table.Column align="center">Key Techniques</Table.Column>
           <Table.Column align="center">Location Found</Table.Column>
+          <Table.Column align="center">View Stats</Table.Column>
         </Table.Header>
         <Table.Body>
           <Table.Row key="1">
-            <Table.Cell>
-            </Table.Cell>
+            <Table.Cell></Table.Cell>
             <Table.Cell>Larbeight</Table.Cell>
             <Table.Cell>Wither Shot, Anit-Nap, Tackle Slip 2</Table.Cell>
             <Table.Cell>Kilika Port - Docks</Table.Cell>
-          </Table.Row>
-          <Table.Row key="2">
-          <Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
             </Table.Cell>
+          </Table.Row>
+
+          <Table.Row key="2">
+            <Table.Cell></Table.Cell>
             <Table.Cell>Isken</Table.Cell>
             <Table.Cell>Wither Pass, Wither Tackle, Wither Tackle 2</Table.Cell>
             <Table.Cell>Kilika Port - House</Table.Cell>
-          </Table.Row>
-          <Table.Row key="3">
-          <Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
             </Table.Cell>
+          </Table.Row>
+
+          <Table.Row key="3">
+            <Table.Cell></Table.Cell>
             <Table.Cell>Vuroja</Table.Cell>
             <Table.Cell>Wither Tackle, Nap Pass, Anti-Nap</Table.Cell>
             <Table.Cell>Kilika Port - Docks</Table.Cell>
-          </Table.Row>
-          <Table.Row key="4">
-          <Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
             </Table.Cell>
+          </Table.Row>
+
+          <Table.Row key="4">
+            <Table.Cell></Table.Cell>
             <Table.Cell>Kulukan</Table.Cell>
             <Table.Cell>Drain Tackle, Nap Pass, Venom Tackle 3</Table.Cell>
             <Table.Cell>Kilika Port - Tavern</Table.Cell>
-          </Table.Row>
-          <Table.Row key="5">
-          <Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
             </Table.Cell>
+          </Table.Row>
+
+          <Table.Row key="5">
+            <Table.Cell></Table.Cell>
             <Table.Cell>Deim</Table.Cell>
             <Table.Cell>Venom Tackle, Wither Pass, Pile Wither</Table.Cell>
             <Table.Cell>Kilika Tempe - Great Hall</Table.Cell>
-          </Table.Row>
-          <Table.Row key="6">
-          <Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
             </Table.Cell>
+          </Table.Row>
+
+          <Table.Row key="6">
+            <Table.Cell></Table.Cell>
             <Table.Cell>Nizarut</Table.Cell>
             <Table.Cell>Venom Shot, Anti-Wither, Anti-Nap</Table.Cell>
             <Table.Cell>Kilika Tempe - Great Hall</Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
+            </Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
--- pages/blitzball/goers.js
+++ pages/blitzball/goers.js
@@ -1,6 +1,6 @@
 import Layout from "../../components/layout";
 import Image from "next/image";
-import { Table } from "@nextui-org/react";
+import { Table, Button } from "@nextui-org/react";
 import { Typography } from "@mui/material";
 
 export default function Aurochs() {
@@ -13,18 +13,22 @@ export default function Aurochs() {
         Team Members
       </Typography>
       <Table
-        aria-label="Example static collection table"
+        aria-label="Goers Team Members table"
         css={{
           height: "auto",
           minWidth: "100%",
         }}
         selectionMode="single"
+        color="secondary"
+        bordered
+        lined
       >
         <Table.Header>
           <Table.Column align="center">Photo</Table.Column>
           <Table.Column align="center">Name</Table.Column>
           <Table.Column align="center">Key Techniques</Table.Column>
           <Table.Column align="center">Location Found</Table.Column>
+          <Table.Column align="center">View Stats</Table.Column>
         </Table.Header>
         <Table.Body>
           <Table.Row key="1">
@@ -32,36 +36,96 @@ export default function Aurochs() {
             <Table.Cell>Bickson</Table.Cell>
             <Table.Cell>Wither Shot, Nap Pass, Anti-Nap</Table.Cell>
             <Table.Cell>Luca Harbor - Number 3 Dock</Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
+            </Table.Cell>
           </Table.Row>
           <Table.Row key="2">
             <Table.Cell></Table.Cell>
             <Table.Cell>Abus</Table.Cell>
             <Table.Cell>Grip Gloves, Venom Tackle 2, Venom Shot 3</Table.Cell>
             <Table.Cell>Luca Harbor - Number 3 Dock</Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
+            </Table.Cell>
           </Table.Row>
           <Table.Row key="3">
             <Table.Cell></Table.Cell>
             <Table.Cell>Graav</Table.Cell>
             <Table.Cell>Venom Pass, Tackle Slip, Drain Tackle 2</Table.Cell>
             <Table.Cell>Luca Harbor - Number 3 Dock</Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
+            </Table.Cell>
           </Table.Row>
           <Table.Row key="4">
             <Table.Cell></Table.Cell>
             <Table.Cell>Doram</Table.Cell>
             <Table.Cell>Wither Tackle, Nap Tackle, Volley Shot</Table.Cell>
             <Table.Cell>Luca Harbor - Number 3 Dock</Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
+            </Table.Cell>
           </Table.Row>
           <Table.Row key="5">
             <Table.Cell></Table.Cell>
             <Table.Cell>Balgerda</Table.Cell>
             <Table.Cell>Nap Tackle, Anti-Wither, Drain Tackle</Table.Cell>
             <Table.Cell>Luca Harbor - Number 3 Dock</Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
+            </Table.Cell>
           </Table.Row>
           <Table.Row key="6">
             <Table.Cell></Table.Cell>
             <Table.Cell>Raudy</Table.Cell>
             <Table.Cell>Grip Gloves, Gamble, Tackle Slip 2</Table.Cell>
             <Table.Cell>Luca Harbor - Number 3 Dock</Table.Cell>
+            <Table.Cell>
+              <Button
+                size="xs"
+                color="secondary"
+                ghost
+                css={{ margin: "auto" }}
+              >
+                Stats
+              </Button>
+            </Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
