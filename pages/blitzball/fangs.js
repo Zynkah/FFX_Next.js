d402bb421ab46441a0a8189383e98535136d4732
Zynkah <ffgurl07@yahoo.com>
2023-08-01 06:25:54 -0500

    Ronso and Guado Blitzball Added

--- pages/blitzball/fangs.js
+++ pages/blitzball/fangs.js
@@ -1,134 +1,32 @@
-import Layout from "../../components/layout";
-import Image from "next/image";
-import { Typography } from "@mui/material";
-import { Table, Button } from "@nextui-org/react";
+import React, { useState, useEffect } from "react";
+import Tables from "../../components/tables";
 
-export default function Aurochs() {
-  return (
-    <Layout>
-      <Typography variant="h4" sx={{ margin: "20px" }}>
-        Ronso Fangs
-      </Typography>
-      <Typography variant="h6" component="h2">
-        Team Members
-      </Typography>
-      <Table
-        aria-label="Fang Team Members table"
-        css={{
-          height: "auto",
-          minWidth: "100%",
-        }}
-        selectionMode="single"
-        color="secondary"
-        bordered
-        lined
-      >
-        <Table.Header>
-          <Table.Column align="center">Photo</Table.Column>
-          <Table.Column align="center">Name</Table.Column>
-          <Table.Column align="center">Key Techniques</Table.Column>
-          <Table.Column align="center">Location Found</Table.Column>
-          <Table.Column align="center">View Stats</Table.Column>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row key="1">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Basik Ronso</Table.Cell>
-            <Table.Cell>Nap Shot, Venom Tackle, Invisible Shot</Table.Cell>
-            <Table.Cell>Luca Harbor - Number 4 Dock</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="2">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Argai Ronso</Table.Cell>
-            <Table.Cell>Wither Tackle, Venom Pass 2, Anti-Drain</Table.Cell>
-            <Table.Cell>Luca Harbor - Number 4 Dock</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="3">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Gazna Ronso</Table.Cell>
-            <Table.Cell>Venom Pass, Drain Tackle, Volley Shot 2</Table.Cell>
-            <Table.Cell>Luca Harbor - Number 4 Dock</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="4">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Nuvy Ronso</Table.Cell>
-            <Table.Cell>Venom Tackle, Volleyh Shot, Tackle Slip</Table.Cell>
-            <Table.Cell>Luca Harbor - Number 4 Dock</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="5">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Irga Ronso</Table.Cell>
-            <Table.Cell>Pile Wither, Wither Tackle 3Super Goalie</Table.Cell>
-            <Table.Cell>Luca Harbor - Number 4 Dock</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="6">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Zamzi Ronso</Table.Cell>
-            <Table.Cell>Spin Ball, Super Goalie, Invisible Shot</Table.Cell>
-            <Table.Cell>Luca Harbor - Number 4 Dock</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-    </Layout>
-  );
-}
+export default function Fangs() {
+  const [team, setTeam] = useState(null);
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const teamname = "Ronso Fangs";
+
+        const response = await fetch(
+          `/api/blitzballData?teamname=${encodeURIComponent(teamname)}`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        setTeam(data.team);
+        setCharacters(data.characters);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return <>{characters && <Tables data={{ characters }} />}</>;
+};
--- pages/blitzball/glories.js
+++ pages/blitzball/glories.js
@@ -1,134 +1,32 @@
-import Layout from "../../components/layout";
-import Image from "next/image";
-import { Typography } from "@mui/material";
-import { Table, Button } from "@nextui-org/react";
+import React, { useState, useEffect } from "react";
+import Tables from "../../components/tables";
 
-export default function Aurochs() {
-  return (
-    <Layout>
-      <Typography variant="h4" sx={{ margin: "20px" }}>
-        Guado Glories
-      </Typography>
-      <Typography variant="h6" component="h2">
-        Team Members
-      </Typography>
-      <Table
-        aria-label="Glories Team Members table"
-        css={{
-          height: "auto",
-          minWidth: "100%",
-        }}
-        selectionMode="single"
-        color="secondary"
-        bordered
-        lined
-      >
-        <Table.Header>
-          <Table.Column align="center">Photo</Table.Column>
-          <Table.Column align="center">Name</Table.Column>
-          <Table.Column align="center">Key Techniques</Table.Column>
-          <Table.Column align="center">Location Found</Table.Column>
-          <Table.Column align="center">View Stats</Table.Column>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row key="1">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Giera Guado</Table.Cell>
-            <Table.Cell>Venom Shot, Nap Shot, Pile Venom</Table.Cell>
-            <Table.Cell>Guadosalam</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="2">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Zazi Guado</Table.Cell>
-            <Table.Cell>Anti-Venom, Wither Shot 2, Anti-Venom 2</Table.Cell>
-            <Table.Cell>Guadosalam - House</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="3">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Navara Guado</Table.Cell>
-            <Table.Cell>Drain Tackle, Super Goalie, Nap Tackle 2</Table.Cell>
-            <Table.Cell>Guadosalam</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="4">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Auda Guado</Table.Cell>
-            <Table.Cell>Anit-Venom, Anti-Nap, Anti-Wither</Table.Cell>
-            <Table.Cell>Guadosalam</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="5">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Pah Guado</Table.Cell>
-            <Table.Cell>Drain Tackle, Venom Tackle 2, Gamble</Table.Cell>
-            <Table.Cell>Guadosalam - House</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row key="6">
-            <Table.Cell></Table.Cell>
-            <Table.Cell>Noy Guado</Table.Cell>
-            <Table.Cell>Anti-Nap, Wither Pass 2, Elite Defense</Table.Cell>
-            <Table.Cell>Guadosalam - Inn</Table.Cell>
-            <Table.Cell>
-              <Button
-                size="xs"
-                color="secondary"
-                ghost
-                css={{ margin: "auto" }}
-              >
-                Stats
-              </Button>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-    </Layout>
-  );
+export default function Glories() {
+  const [team, setTeam] = useState(null);
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const teamname = "Guado Glories";
+
+        const response = await fetch(
+          `/api/blitzballData?teamname=${encodeURIComponent(teamname)}`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+        setTeam(data.team);
+        setCharacters(data.characters);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return <>{characters && <Tables data={{ characters }} />}</>;
 }
