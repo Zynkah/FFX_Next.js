7f83f4fbc074fb3cc7e261c7f52d7f7a5be03b49
Zynkah <ffgurl07@yahoo.com>
2023-08-08 02:07:11 -0500

    fixed

--- pages/characters.js
+++ pages/characters.js
@@ -1,9 +1,50 @@
 import React, { useState, useEffect } from "react";
 import CharacterCards from "../components/characterCard";
+import Layout from "../components/layout";
+import { Typography, Box, Tabs, Tab } from "@mui/material";
+import PropTypes from "prop-types";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 export default function Characters() {
   const [mainCharacters, setMainCharacters] = useState(null);
   const [supportingCharacters, setSupportingCharacters] = useState(null);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   useEffect(() => {
     async function fetchMainCharacters() {
@@ -49,10 +90,43 @@ export default function Characters() {
   }, []);
 
   return (
-    <>
-      {mainCharacters && <CharacterCards data={mainCharacters} />}
-      {supportingCharacters && <CharacterCards data={supportingCharacters} />}
-    </>
+    <Layout>
+      <Typography variant="h4" sx={{ margin: "20px" }}>
+        Characters
+      </Typography>
+      <Box sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: "divider",
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons
+            aria-label="basic tabs"
+            textColor="secondary"
+            indicatorColor="secondary"
+          >
+            <Tab label="Main Characters" {...a11yProps(0)} />
+            <Tab label="Supporting Characters" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          {mainCharacters && <CharacterCards data={mainCharacters} />}
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          {supportingCharacters && (
+            <CharacterCards data={supportingCharacters} />
+          )}
+        </TabPanel>
+      </Box>
+    </Layout>
   );
 }
 
